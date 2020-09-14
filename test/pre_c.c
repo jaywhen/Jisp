958d80c4b0ddd2e16906a9ff05715b5e848d0259
jaywhen <jaywhenxiang@foxmail.com>
2020-09-15 00:29:08 +0800

    add more dummy test'

--- test/pre_c.c
+++ test/pre_c.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
-
+#include <string.h>
 static char input[2048];
 
+#define QUIT "quit()\n"
+
 int main(int argc, char** argv) {
-    printf("%ld\n", sizeof(input));
+    puts("\nJisp Version 0.0.0.0.1");
+    puts("Press CTRL + c || \"quit()\" to Exit\n");
+    while(1) {
+        fputs("Jisp> ", stdout);
+        fgets(input, 2048, stdin);
+        
+        if(!strcmp(input, QUIT)) {
+            puts("bye!");
+            break;
+        }
+        puts("Yes!You are right!");
+    }
+
+    return 0;
 }
