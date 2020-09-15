807910c48715b7e18ff5a30dd72cdfe0c921f7b5
jaywhen <jaywhenxiang@foxmail.com>
2020-09-15 23:46:02 +0800

    add a linux version

--- test/pre_c.c
+++ test/pre_c.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
-#include <string.h>
-static char input[2048];
+#include <stdlib.h> /* free() */
+#include <string.h> /* strcmp() */
+#include <editline/readline.h> /* readline() */
+#include <editline/history.h>  /* add_history() */
 
-#define QUIT "quit()\n"
+// static char input[2048];
+
+#define QUIT "quit()"
 
 int main(int argc, char** argv) {
     puts("\nJisp Version 0.0.0.0.1");
     puts("Press CTRL + c || \"quit()\" to Exit\n");
     while(1) {
-        fputs("Jisp> ", stdout);
-        fgets(input, 2048, stdin);
-        
+        // fputs("Jisp> ", stdout);
+        // fgets(input, 2048, stdin);
+        char* input = readline(">Jisp ");
+        add_history(input);
+
         if(!strcmp(input, QUIT)) {
             puts("bye!");
             break;
         }
         puts("Yes!You are right!");
+        free(input);
     }
 
     return 0;
