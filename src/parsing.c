3978b617586c7b432e91d30c2f5aa06e42fe7014
jaywhen <jaywhenxiang@foxmail.com>
2020-09-22 22:19:58 +0800

    can parse simple polish expr

--- src/parsing.c
+++ src/parsing.c
@@ -1,7 +1,8 @@
+#include "mpc.h"
 #include <stdio.h>
 #include <stdlib.h> /* free() */
 #include <string.h> /* strcmp() */
-#include "mpc.h"
+
 #ifdef _WIN32
 static char buffer[2048];
 /* the fake readline function */
@@ -25,8 +26,27 @@ void add_history(char* unused){}
 #define QUIT "quit()"
 
 int main(int argc, char** argv) {
+    /* create some parsers */
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Jisp     = mpc_new("jisp");
+
+    puts("boring");
+
+    mpca_lang(MPCA_LANG_DEFAULT, 
+        "                                                \
+            number: /-?[0-9]+/;                          \
+            operator: '+' | '-' | '*' | '/';             \
+            expr: <number> | '(' <operator> <expr>+ ')'; \
+            jisp: /^/ <operator> <expr>+ /$/;            \
+        ",
+    Number, Operator, Expr, Jisp
+    );
+    
     puts("\nJisp Version 0.0.0.0.1");
     puts("Press CTRL + c || \"quit()\" to Exit\n");
+
     while(1) {
         // fputs("Jisp> ", stdout);
         // fgets(input, 2048, stdin);
@@ -37,10 +57,21 @@ int main(int argc, char** argv) {
             puts("bye!");
             break;
         }
-        puts("Yeah! You are right!");
+
+        mpc_result_t r;
+        if(mpc_parse("<stdin>", input, Jisp, &r)) {
+            /* if success print the AST */
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            /* otherwise print the error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
         free(input);
     }
 
+    mpc_cleanup(4, Number, Operator, Expr, Jisp);
+
     return 0;
 }
-
